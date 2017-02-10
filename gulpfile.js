536a82733e0b12366f928f384a0a73fb16db4f4f
Glynnis Ritchie <gritchie@boundlessgeo.com>
2017-02-10 21:29:16 +0000

    Add Modernizr and gulp-modernizr

--- gulpfile.js
+++ gulpfile.js
@@ -2,7 +2,8 @@
 var gulp = require('gulp');
 var less = require('gulp-less');
 var path = require('path');
- 
+var modernizr = require('gulp-modernizr');
+
 gulp.task('less', function () {
   return gulp.src('./style/less/test.less')
     .pipe(less({
@@ -11,7 +12,13 @@ gulp.task('less', function () {
     .pipe(gulp.dest('./style/css'));
 });
 
+gulp.task('modernizr', function() {
+  gulp.src('./js/*.js')
+    .pipe(modernizr())
+    .pipe(gulp.dest("build/"))
+});
+
 gulp.task('watch', function(){
   gulp.watch('./style/less/*.less', ['less']);
   // Other watchers
-})
+});
